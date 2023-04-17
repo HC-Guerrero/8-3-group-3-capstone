12b6ff5ddbc992035c5e2badf295879da544e795
H.C.Guerrero <clydeguerrero1@gmail.com>
2023-04-17 00:17:06 -0400

    comment out userprofile.js filter

--- frontend/src/Components/UserProfile.js
+++ frontend/src/Components/UserProfile.js
@@ -10,13 +10,13 @@ export default function UserProfile({ currentFilter, users, user }) {
   return (
     <div className='UserGallery bg-beige'>
       {users
-        .filter((u) => {
+        /*.filter((u) => {
           if (!currentFilter) {
             return true;
           } else {
             return u.dietary_restrictions.includes(currentFilter);
           }
-        })
+        }) */
         .map((u) => {
           return (
             <User info={u} key={`${u.id}-${u.name}`} signedInUser={user} />
